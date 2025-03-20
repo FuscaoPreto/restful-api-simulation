932322249cee32482699483b1ba7087bfe977290
FuscaoPreto <mywork.anon@protonmail.com>
2025-03-20 17:04:24 -0300

    init

---
+++ src/main/java/com/fusca/restful_api_simulation/RestfulApiSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.fusca.restful_api_simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fusca/restful_api_simulation/RestfulApiSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fusca.restful_api_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
